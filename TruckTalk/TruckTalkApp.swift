4a170ebadbd3af3c84d28153bce268e06b88baab
senlik <cr7abdul.as@gmail.com>
2025-06-18 02:23:58 -0500

    Initial Commit

---
+++ TruckTalk/TruckTalkApp.swift
@@ -0,0 +1,17 @@
+//
+//  TruckTalkApp.swift
+//  TruckTalk
+//
+//  Created by Senlik on 6/18/25.
+//
+
+import SwiftUI
+
+@main
+struct TruckTalkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TruckTalkTests/TruckTalkTests.swift
@@ -0,0 +1,17 @@
+//
+//  TruckTalkTests.swift
+//  TruckTalkTests
+//
+//  Created by Senlik on 6/18/25.
+//
+
+import Testing
+@testable import TruckTalk
+
+struct TruckTalkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
